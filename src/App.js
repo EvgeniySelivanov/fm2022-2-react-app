0919576fd92eaa5cacf9a3b5ba0fd30184f21dad
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-02-16 11:00:50 +0200

    add sign up form with reducer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import FuncHeader from './components/FuncHeader';
 import HomePage from './pages/HomePage';
 import { UserContext, ThemeContext } from './contexts';
 import CONSTANTS from './constants';
-const{ THEMES }= CONSTANTS;
+import { useClicker } from './hooks';
+import SignUpForm from './components/forms/SignUpForm';
+const { THEMES } = CONSTANTS;
 
 function App() {
   const [user, setUser] = useState({
     id: 1,
     name: 'Ivan Petrov'
-  })
-  const [theme, setTheme] = useState(THEMES.LIGHT)
+  });
+  const [theme, setTheme] = useState(THEMES.LIGHT);
+  const count = useClicker(300);
 
 
   return (
     <>
-    <ThemeContext.Provider value={[theme, setTheme]}>
-      <UserContext.Provider value={[user, setUser]}>
-        <BrowserRouter>
-          <FuncHeader />
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
+      <ThemeContext.Provider value={[theme, setTheme]}>
+        <UserContext.Provider value={[user, setUser]}>
+          <BrowserRouter>
+            <FuncHeader />
+            <h2>Count clicker= {count}</h2>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to='/'>Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to='/sign-up'>SignUp</NavLink>
+                </li>
+              </ul>
+            </nav>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/sign-up' element={<SignUpForm />} />
+            </Routes>
+          </BrowserRouter>
+        </UserContext.Provider>
       </ThemeContext.Provider>
 
 
---
+++ src/hooks/useClicker.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+const useClicker = (value=0) => {
+  const [count, setCount] = useState(value);
+  useEffect(() => {
+    console.log('add effect');
+    const handlerClick = () => {
+      setCount((prevCount) => prevCount+1);
+    }
+    //add effect (component didMount if [] clear array!!!! second parametr )
+    window.addEventListener('click', handlerClick);
+    return () => {
+      console.log('remove effect');
+      //clean effect  (component Will-Unmount if [] clear array!!!! second parametr)
+      window.removeEventListener('click', handlerClick);
+    };//eslint-disable-next-line
+  }, []);
+
+  return  count ;
+ 
+}
+
+export default useClicker;
